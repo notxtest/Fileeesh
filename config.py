42bf31d6a8f11c6f49c093d2ac00a523ef6563cd
notxtest <timepassxnot@gmail.com>
2026-08-26 22:19:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,53 +1,39 @@
-import os
 import logging
 from logging.handlers import RotatingFileHandler
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
-PORT = os.getenv("PORT", "5010")
-OWNER_ID = int(os.getenv("OWNER_ID", "0123456789"))
+PORT = '5010'
+OWNER_ID = 7171541681
 
-MSG_EFFECT = 5104841245755180586
+MSG_EFFECT = 5046509860389126442
 
-SHORT_URL = os.getenv("SHORT_URL", "linkshortify.com")
-SHORT_API = os.getenv("SHORT_API", "")
-SHORT_TUT = "https://t.me/How_to_Download_7x/26"
+SHORT_URL = "linkshortify.com" # shortner url 
+SHORT_API = "" 
+SHORT_TUT = "https://t.me/+5qAwb0OP6-02MTdl"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = os.getenv("BOT_TOKEN", "8966687209:AAEKwbScmqL57ZNU4zzxFPSSGVXAUczcJ6E")
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
+TOKEN = "8966687209:AAHM3X1grcK1RNREPD9vvU0ebA9uFPLTl7c"
+API_ID = "23640310"
+API_HASH = "079f8339732e35e032a64ee020e0b90b"
 WORKERS = 5
 
-DB_URI = os.getenv("DB_URI", "mongodb")
-DB_NAME = os.getenv("DB_NAME", "yato")
+DB_URI = "mongodb+srv://krishnaonly999:Krishdiya07@cluster0.h4rzpxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "Cluster0"
 
-# Force Subscription Channels
-FSUB_CHANNEL_ID = int(os.getenv("FSUB_CHANNEL_ID", "-1004451251761"))
-FSUB_REQUEST_ENABLED = os.getenv("FSUB_REQUEST_ENABLED", "True").lower() == "true"
-FSUB_TIMER_MINUTES = int(os.getenv("FSUB_TIMER_MINUTES", "10"))
-FSUBS = [[FSUB_CHANNEL_ID, FSUB_REQUEST_ENABLED, FSUB_TIMER_MINUTES]]
-
+FSUBS = [[-1004368668083, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL = os.getenv("DB_CHANNEL", "")  # just channel id as string
-
+DB_CHANNEL = -1004451251761   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
 #     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
-
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
-
-# Admin IDs (from env)
-ADMINS = [int(admin_id) for admin_id in os.getenv("ADMINS", "").split(",") if admin_id.strip()]
-
+# Admin IDs
+ADMINS = [7171541681, 7171541681]
 # Bot Settings
 DISABLE_BTN = True
 PROTECT = True
@@ -56,7 +42,7 @@ PROTECT = True
 MESSAGES = {
     "START": "<b>›› ʜᴇʏ!!, {first} ~ <blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ʙᴏᴛ. ɪ ᴀᴍ ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ꜰɪɴᴅ ᴀɴᴅ ɢᴇᴛ ʏᴏᴜʀ ꜰɪʟᴇs ᴇᴀsɪʟʏ ᴀɴᴅ ǫᴜɪᴄᴋʟʏ.</blockquote></b>",
     "FSUB": "<b><blockquote>›› ʜᴇʏ ×</blockquote>\n  ʏᴏᴜʀ ғɪʟᴇ ɪs ʀᴇᴀᴅʏ ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ғɪʟᴇs</b>",
-    "ABOUT": "<b>›› ᴀʙᴏᴜᴛ ᴛʜɪs ʙᴏᴛ\n\n<blockquote expandable>🤖 ʙᴏᴛ ɴᴀᴍᴇ: CineVinesFileBot\n👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: 🐦\n🐍 ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a>\n📚 ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a>\n🗄️ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a>\n\nᴛʜɪs ʙᴏᴛ ɪs ᴍᴀᴅᴇ ꜰᴏʀ ꜰɪʟᴇ sʜᴀʀɪɴɢ ᴘᴜʀᴘᴏsᴇs ᴏɴʟʏ.</blockquote></b>",
+    "ABOUT": "<b>›› ᴀʙᴏᴜᴛ ᴛʜɪs ʙᴏᴛ\n\n<blockquote expandable>🤖 ʙᴏᴛ ɴᴀᴍᴇ: Fɪʟᴇ Sʜᴀʀɪɴɢ Bᴏᴛ\n👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: 🐦\n🐍 ʟᴀɴɢᴜᴀɢᴇ: <a href='https://docs.python.org/3/'>Pʏᴛʜᴏɴ 3</a>\n📚 ʟɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ ᴠ2</a>\n🗄️ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>Mᴏɴɢᴏ ᴅʙ</a>\n\nᴛʜɪs ʙᴏᴛ ɪs ᴍᴀᴅᴇ ꜰᴏʀ ꜰɪʟᴇ sʜᴀʀɪɴɢ ᴘᴜʀᴘᴏsᴇs ᴏɴʟʏ.</blockquote></b>",
     "REPLY": "<b>For More Join - @CineVines</b>",
     "SHORT_MSG": "<b>📊 ʜᴇʏ {first}, \n\n‼️ ɢᴇᴛ ᴀʟʟ ꜰɪʟᴇꜱ ɪɴ ᴀ ꜱɪɴɢʟᴇ ʟɪɴᴋ ‼️\n\n ⌯ ʏᴏᴜʀ ʟɪɴᴋ ɪꜱ ʀᴇᴀᴅʏ, ᴋɪɴᴅʟʏ ᴄʟɪᴄᴋ ᴏɴ ᴏᴘᴇɴ ʟɪɴᴋ ʙᴜᴛᴛᴏɴ..</b>",
     "START_PHOTO": "https://graph.org/file/b9f294b8549d8803e2bae-1a97a4394146e77a7b.jpg",
